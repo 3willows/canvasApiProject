1ec7f6c415717c27212e228fc31d784dd7aa75b5
Jasper Wong <wlcjasper@gmail.com>
2023-12-03 12:11:35 +0800

    add walls

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-})
+const walls =[
+  Bodies.rectangle(400, 0, 800, 50, {
+    isStatic:true
+  }),
+  Bodies.rectangle(400, 600, 800, 50, {
+    isStatic:true
+  }),
+  Bodies.rectangle(0, 300, 50, 600, {
+    isStatic:true
+  }),
+  Bodies.rectangle(800, 300, 50, 600, {
+    isStatic:true
+  })
+];
 
-World.add(world, shape);
+World.add(world, walls);
