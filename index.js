7d325b03e6f9d56805a5d28653416c5c867a38dd
Jasper Wong <wlcjasper@gmail.com>
2023-12-03 12:17:14 +0800

    Make draggable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Engine, Render, Runner, World, Bodies} = Matter;
+const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
 const engine = Engine.create();
 const {world} = engine;
@@ -14,6 +14,10 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}))
 
 const walls =[
   Bodies.rectangle(400, 0, 800, 50, {
@@ -31,3 +35,5 @@ const walls =[
 ];
 
 World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
